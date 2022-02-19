e963166fd809c1272e833d3477639c596ba3620a
Abdul-Rozaq <64203874+Abdul-Rozaq@users.noreply.github.com>
2022-02-19 19:32:01 +0100

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            // create a scope to host services. When we run app, the scope gets disposed after our app starts successfully
+            using var scope = host.Services.CreateScope(); 
+
+            var services = scope.ServiceProvider; // get access to our services in startup.cs
+
+            try
+            {
+                var context = services.GetRequiredService<DataContext>(); // fetch database context
+                await context.Database.MigrateAsync(); // create a DB if DB isn't created
+
+                await Seed.SeedData(context);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during migration");
+            }
+
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
